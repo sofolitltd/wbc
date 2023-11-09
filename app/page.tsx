a11087f786f7ff6d6a64d5071025a8c4b0179065
sofolitltd <sofolitltd@gmail.com>
2023-11-09 15:42:05 +0600

    fix nav, and responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Blogs from "./components/Blogs";
 export default function Home() {
   return (
     <>
+    {/* <div>
       <Head>
         <title>"Wellbeing Clinic"</title>
         <meta
@@ -13,6 +14,7 @@ export default function Home() {
           content="Wellbeing Clinic is a mental health service provider offering a comprehensive range of services for optimal mental health. We provide personalized, compassionate care that addresses your unique needs and goals."
         />
       </Head>
+      </div> */}
       <main className="px-4 sm:px-6 md:px-24">
         {/* hero */}
         <Hero />
